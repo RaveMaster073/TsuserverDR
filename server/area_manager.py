60664f308517a2c8e7204100a69e5a90c8870423
RaveMaster073 <mrmathematician01@gmail.com>
2020-03-20 12:16:00 -0700

    Fix /bg turning on lights in previously dark rooms.

--- server/area_manager.py
+++ server/area_manager.py
@@ -224,7 +224,11 @@ class AreaManager:
             if validate and bg.lower() not in [name.lower() for name in self.server.backgrounds]:
                 raise AreaError('Invalid background name.')
 
-            self.background = bg
+            if self.lights:
+                self.background = bg
+            else:
+                self.background = self.server.config['blackout_background']
+                self.background_backup = bg
             for c in self.clients:
                 if c.is_blind and not override_blind:
                     c.send_command('BN', self.server.config['blackout_background'])
