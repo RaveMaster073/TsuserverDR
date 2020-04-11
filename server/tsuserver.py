3944601df409de2ccc6aecfb7167b09fbc0c77bd
Chrezm <42015761+Chrezm@users.noreply.github.com>
2020-04-10 21:36:28 -0400

    Update internal version with correct date

--- server/tsuserver.py
+++ server/tsuserver.py
@@ -43,7 +43,7 @@ class TsuserverDR:
         self.major_version = 3
         self.minor_version = 0
         self.segment_version = 'a24'
-        self.internal_version = 'M200409a'
+        self.internal_version = 'M200410a'
         version_string = self.get_version_string()
         self.software = 'TsuserverDR {}'.format(version_string)
         self.version = 'TsuserverDR {} ({})'.format(version_string, self.internal_version)
